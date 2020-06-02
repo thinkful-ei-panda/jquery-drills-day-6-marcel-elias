f81d0c9f2c4f6bd0b55c5f83e025dd3ad5af2b21
Elias Martinez <jemart218@gmail.com>
2020-06-02 12:19:07 -0700

    ca t carousel done

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
+'use strict';
+
+function handleThumbnailClicks() {
+  $('.thumbnail').click(event =>{ 
+    
+    const thumbImg = $(event.currentTarget).find('img').attr('src');
+
+    $('.hero img').attr('src', thumbImg );
+
+    const thumbAlt = $(event.currentTarget).find('img').attr('alt');
+
+    $('.hero alt') .attr('alt',thumbAlt );
+    
   });
+}
+
+$(handleThumbnailClicks)
+
   
