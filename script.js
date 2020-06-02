1543e364c6dc1c8ae5d2c61d545cff8626b39ec3
Elias Martinez <jemart218@gmail.com>
2020-06-02 14:24:46 -0700

    fizzbuzz minus the form server

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function handleSubmissions() {
+  $('#number-chooser').submit(event => {
+    event.preventDefault();
+
+    $('.js-results').empty();
+
+    const numberChoice = $(event.currentTarget).find('input[name="number-choice"]').val();
+    const results = [];
+
+    for (let i=0;i<=numberChoice;i++) {
+      if (numberChoice % 15 === 0) {
+        results.push($('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'));
+      }
+
+      else if (numberChoice % 5 === 0) {
+        results.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      }
+
+      else if (numberChoice % 3 === 0) {
+        results.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>'));
+      }
+
+      else {
+        results.push($(`<div clas="fizz-buzz-item"><span>${i}</span></div>`));
+      }
+    }
+    
+    $('.js-results').append(results);
+
+  })
+
+
+
+}
